f2c702f791b4939c28c4a5468661f7716cab95c2
Alex Chen <axchen7@outlook.com>
2024-11-24 18:17:08 -0500

    Baud as const

--- main/main.cpp
+++ main/main.cpp
@@ -2,10 +2,12 @@
 
 #include "device.h"
 
+const unsigned long kSerialBaud = 115200;
+
 extern "C" void app_main() {
     initArduino();
 
-    Serial.begin(115200);
+    Serial.begin(kSerialBaud);
 
     device::InitAll();
 
