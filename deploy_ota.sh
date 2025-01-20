9d13819a20319a9ce7b5b035590a7d1c8d5795ec
Alex Chen <axchen7@outlook.com>
2025-01-20 16:28:44 -0500

    Add ota script

---
+++ deploy_ota.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+REMOTE_HOST=csiwiki
+REMOTE_ASSETS_DIR=/home/wikijs/esp-ota-server/assets
+
+# Generate a random short-sha version
+VERSION=$(openssl rand -hex 4)
+
+# Write the version into ota_version.h
+echo "#define OTA_VERSION \"$VERSION\"" > main/include/ota_version.h
+
+# Write version.txt to the remote assets directory
+ssh $REMOTE_HOST "echo $VERSION > $REMOTE_ASSETS_DIR/version.txt"
+
+# Build and copy the binary to the remote assets directory
+source activate
+idf.py build
+scp build/avionics-software-2024-2025.bin $REMOTE_HOST:$REMOTE_ASSETS_DIR/program.bin
+
+echo "Deployed OTA version: $VERSION"
