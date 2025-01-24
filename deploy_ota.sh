1acfb34cb322d35143d553affec5088d8cd63e99
Alex Chen <axchen7@outlook.com>
2025-01-24 05:48:18 -0500

    Fix writing version order

--- deploy_ota.sh
+++ deploy_ota.sh
@@ -11,12 +11,12 @@ VERSION=$(openssl rand -hex 4)
 # Write the version into ota_version.h
 echo "#define OTA_VERSION \"$VERSION\"" > main/include/ota_version.h
 
-# Write version.txt to the remote assets directory
-ssh $REMOTE_HOST "echo $VERSION > $REMOTE_ASSETS_DIR/version.txt"
-
 # Build and copy the binary to the remote assets directory
 source activate
 idf.py build
 scp build/avionics-software-2024-2025.bin $REMOTE_HOST:$REMOTE_ASSETS_DIR/program.bin
+
+# Write version.txt to the remote assets directory
+ssh $REMOTE_HOST "echo $VERSION > $REMOTE_ASSETS_DIR/version.txt"
 
 echo "Deployed OTA version: $VERSION"
