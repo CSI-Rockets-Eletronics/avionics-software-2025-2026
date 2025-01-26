8aee4348a90a3fadc6aa20d5663b8701f4742d27
Alex Chen <axchen7@outlook.com>
2025-01-25 22:18:08 -0500

    Uncomment dht

--- main/devices/dev_dht.h
+++ main/devices/dev_dht.h
@@ -5,23 +5,30 @@
 
 using namespace avionics;
 
-static const int kDhtPin = 99;  // TODO
+static const int kDhtPin = 6;
 
 class DevDht : public Device {
    public:
-    void Setup() override {
-        // dht.begin();
-    }
+    void Setup() override { dht.begin(); }
 
     void Loop() override {
+        delay(1000);  // DHT11 needs at least 1s between reads
+
+        // force read once for both temperature and humidity
+        bool success = dht.read(true);
+
+        if (!success) {
+            Serial.println("Failed to read from DHT sensor!");
+            return;
+        }
+
         DhtPacket dht_packet{
             .ts = micros(),
-            .temperature = 0.0,
-            .humidity = 0.0,
+            // these will use the values from the last read()
+            .temperature = dht.readTemperature(),
+            .humidity = dht.readHumidity(),
         };
         Send(DeviceType::DevPiSerial, dht_packet);
-
-        delay(1000);
     }
 
    private:
