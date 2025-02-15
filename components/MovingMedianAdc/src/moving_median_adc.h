fd856581d56c009c56c8f17507ec2fd38c10208f
Alex Chen <axchen7@outlook.com>
2025-02-15 18:57:01 -0500

    Tweak adc

--- components/MovingMedianAdc/src/moving_median_adc.h
+++ components/MovingMedianAdc/src/moving_median_adc.h
@@ -3,7 +3,7 @@
 
 #include <Adafruit_ADS1X15.h>
 
-#include "utils.h"
+namespace moving_median_adc {
 
 enum class ADCMode {
     SingleEnded_0,
@@ -18,77 +18,88 @@ enum class ADCMode {
 template <typename ADCType>
 class MovingMedianADC {
    public:
-    MovingMedianADC(const char* debugName, int windowSize, ADCType& adc,
-                    ADCMode mode)
-        : debugName(debugName),
-          adc(adc),
-          mode(mode),
-          medianVolts(windowSize, 0) {}
+    MovingMedianADC(const char* debug_name, ADCMode mode, uint16_t rate,
+                    adsGain_t gain, bool continuous, int window_size)
+        : debug_name{debug_name},
+          mode{mode},
+          continuous{continuous},
+          median_volts{window_size} {
+        adc.setDataRate(rate);
+        adc.setGain(gain);
 
-    void enableContinuous() {
-        uint16_t mux;
-
-        switch (mode) {
-            case ADCMode::SingleEnded_0:
-                mux = ADS1X15_REG_CONFIG_MUX_SINGLE_0;
-                break;
-            case ADCMode::SingleEnded_1:
-                mux = ADS1X15_REG_CONFIG_MUX_SINGLE_1;
-                break;
-            case ADCMode::SingleEnded_2:
-                mux = ADS1X15_REG_CONFIG_MUX_SINGLE_2;
-                break;
-            case ADCMode::SingleEnded_3:
-                mux = ADS1X15_REG_CONFIG_MUX_SINGLE_3;
-                break;
-            case ADCMode::Differential_0_1:
-                mux = ADS1X15_REG_CONFIG_MUX_DIFF_0_1;
-                break;
-            case ADCMode::Differential_2_3:
-                mux = ADS1X15_REG_CONFIG_MUX_DIFF_2_3;
-                break;
+        if (!adc.begin()) {
+            PrintDebugTag();
+            Die("ADS1115 begin failed");
         }
 
-        adc.startADCReading(mux, true);
-        continuous = true;
+        if (continuous) {
+            uint16_t mux;
+
+            switch (mode) {
+                case ADCMode::SingleEnded_0:
+                    mux = ADS1X15_REG_CONFIG_MUX_SINGLE_0;
+                    break;
+                case ADCMode::SingleEnded_1:
+                    mux = ADS1X15_REG_CONFIG_MUX_SINGLE_1;
+                    break;
+                case ADCMode::SingleEnded_2:
+                    mux = ADS1X15_REG_CONFIG_MUX_SINGLE_2;
+                    break;
+                case ADCMode::SingleEnded_3:
+                    mux = ADS1X15_REG_CONFIG_MUX_SINGLE_3;
+                    break;
+                case ADCMode::Differential_0_1:
+                    mux = ADS1X15_REG_CONFIG_MUX_DIFF_0_1;
+                    break;
+                case ADCMode::Differential_2_3:
+                    mux = ADS1X15_REG_CONFIG_MUX_DIFF_2_3;
+                    break;
+            }
+
+            adc.startADCReading(mux, true);
+        }
     }
 
     // returns new zero volts
-    float recalibrate(int sampleCount) {
+    float Recalibrate(int sample_count) {
         std::vector<float> samples;
-        for (int i = 0; i < sampleCount; i++) {
-            samples.push_back(readVolts());
+        for (int i = 0; i < sample_count; i++) {
+            samples.push_back(ReadVolts());
         }
-        float newZeroVolts = utils::median(samples);
+        float newZeroVolts = Median(samples);
         setZero(newZeroVolts);
         return newZeroVolts;
     }
 
-    void resetZero() { setZero(0); }
+    void ResetZero() { SetZero(0); }
 
-    void setZero(float newZeroVolts) {
-        zeroVolts = newZeroVolts;
-        medianVolts.reset(newZeroVolts);
-        printSetZeroVolts();
-    }
+    float GetLatestVolts() { return median_volts.GetLatest() - zero_volts; }
 
-    float getLatestVolts() { return medianVolts.getLatest() - zeroVolts; }
-
-    float getMedianVolts() { return medianVolts.getMedian() - zeroVolts; }
+    float GetMedianVolts() { return median_volts.GetMedian() - zero_volts; }
 
     // polls ADC for a new reading and saves it
-    void tick() { medianVolts.add(readVolts()); }
+    void Tick() { median_volts.Add(ReadVolts()); }
 
    private:
-    const char* debugName;
-    ADCType& adc;
+    const char* debug_name;
     const ADCMode mode;
+    const bool continuous;
 
-    bool continuous = false;
-    float zeroVolts = 0;
-    utils::MovingMedian<float> medianVolts;
+    ADCType adc;
+    MovingMedian<float> median_volts;
+    float zero_volts = 0;
 
-    float readVolts() {
+    void SetZero(float new_zero_volts) {
+        zero_volts = new_zero_volts;
+        median_volts.Reset(new_zero_volts);
+
+        PrintDebugTag();
+        print("Zero set to ");
+        print(zero_volts);
+        println(" volts");
+    }
+
+    float ReadVolts() {
         int16_t counts;
 
         if (continuous) {
@@ -119,12 +130,56 @@ class MovingMedianADC {
         return adc.computeVolts(counts);
     }
 
-    void printSetZeroVolts() {
-        Serial.print("Set zero to ");
-        Serial.print(zeroVolts);
-        Serial.print("volts for ");
-        Serial.println(debugName);
+    void PrintDebugTag() {
+        Serial.print(debug_name);
+        Serial.print(": ");
     }
 };
+
+// returns 0 if the vector is empty
+template <typename T>
+T Median(const std::vector<T>& a) {
+    if (a.empty()) {
+        return 0;
+    }
+
+    std::vector<T> b = a;
+    int index = b.size() / 2;
+    std::nth_element(b.begin(), b.begin() + index, b.end());
+    return b[index];
+}
+
+template <typename T>
+class MovingMedian {
+   public:
+    MovingMedian(int window_size, T initial_value = 0)
+        : window_size{window_size} {
+        for (int i = 0; i < window_size; i++) {
+            values.push_back(initial_value);
+        }
+    }
+
+    void Reset(T initial_value = 0) {
+        for (int i = 0; i < window_size; i++) {
+            values[i] = initial_value;
+        }
+    }
+
+    void Add(T value) {
+        // `values` is always kept at `window_size` elements
+        values.erase(values.begin());
+        values.push_back(value);
+    }
+
+    T GetMedian() { return Median(values); }
+
+    T GetLatest() { return values.back(); }
+
+   private:
+    const int window_size;
+    std::vector<T> values;
+};
+
+}  // namespace moving_median_adc
 
 #endif  // MOVING_MEDIAN_ADC_H_
--- main/devices/dev_fs_transducers.h
+++ main/devices/dev_fs_transducers.h
@@ -1,14 +1,41 @@
+#include <moving_median_adc.h>
+
 #include "avionics.h"
 
 using namespace avionics;
+using namespace moving_median_adc;
 
 class DevFsTransducers : public Device {
    public:
     void Setup() override {}
 
-    void Loop() override {}
+    void Loop() override {
+        gn2_manifold_1_adc.Tick();
+        gn2_manifold_2_adc.Tick();
+
+        Serial.print("gn2_manifold_1_adc: ");
+        Serial.print(gn2_manifold_1_adc.GetMedianVolts());
+        Serial.println(" volts");
+
+        Serial.print("gn2_manifold_2_adc: ");
+        Serial.print(gn2_manifold_2_adc.GetMedianVolts());
+        Serial.println(" volts");
+    }
 
    private:
+    const bool kContinuous = true;
+    const int kWindowSize = 50;
+
+    MovingMedianADC<Adafruit_ADS1115> gn2_manifold_1_adc{
+        "gn2_manifold_1_adc", ADCMode::SingleEnded_1,
+        RATE_ADS1115_860SPS,  GAIN_TWOTHIRDS,
+        kContinuous,          kWindowSize,
+    };
+    MovingMedianADC<Adafruit_ADS1115> gn2_manifold_2_adc{
+        "gn2_manifold_2_adc", ADCMode::SingleEnded_1,
+        RATE_ADS1115_860SPS,  GAIN_TWOTHIRDS,
+        kContinuous,          kWindowSize,
+    };
 };
 
 REGISTER_AVIONICS_DEVICE(DevFsTransducers);
