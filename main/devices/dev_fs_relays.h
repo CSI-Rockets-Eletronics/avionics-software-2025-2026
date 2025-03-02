d987e9de204c1e18ff15c561228ab92155b8235f
Alex Chen <axchen7@outlook.com>
2025-03-02 00:13:28 -0500

    Longer delay

--- main/devices/dev_fs_relays.h
+++ main/devices/dev_fs_relays.h
@@ -5,7 +5,7 @@ using namespace avionics;
 class DevFsRelays : public Device {
    private:
     const int kRelayPins[12] = {5, 1, 6, 7, 15, 16, 17, 18, 8, 2, 12, 9};
-    const int kDelayMs = 1000;
+    const int kDelayMs = 5000;
 
    public:
     void Setup() override {
@@ -19,11 +19,13 @@ class DevFsRelays : public Device {
             digitalWrite(kRelayPins[i], HIGH);
         }
         delay(kDelayMs);
+        Serial.println("High");
 
         for (int i = 0; i < 12; i++) {
             digitalWrite(kRelayPins[i], LOW);
         }
         delay(kDelayMs);
+        Serial.println("Low");
     }
 };
 
